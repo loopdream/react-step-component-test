1221e5ee8978d65ae60cdba009049dc3d9ebeabf
Loopdream <dom@loopdream.com>
2017-11-27 10:33:15 +0000

    changed test

--- src/components/StepCounter/StepCounter.test.js
+++ src/components/StepCounter/StepCounter.test.js
@@ -24,25 +24,28 @@ describe('<StepCounter />', () => {
     expect(numSteps).toBeLessThanOrEqual(5)
   })
   
-  test('the state changes when item clicked', () => {
-    const initialState = 0
-    const wrapper = mount(<StepCounter steps={counterSteps} activeStep={initialState} />)
-    const step2 = wrapper.find('.StepCounterItem__bullet').at(1)
-    step2.simulate('click')
-    expect(wrapper.state('activeStep')).toBe(initialState+1)
-  })
-
-  test('the state shouldnt change if a clicked step isnt an immediate sibling', () => {
+  test('the state changes only when imediate sibling items are clicked', () => {
     const counterSteps = ['item1', 'item2', 'item3', 'item4', 'item5'] // set up 5 steps
-    const initialState = 2 // initial active state to be third step
+    let initialState = 2 // initial active state to be third step
     const wrapper = mount(<StepCounter steps={counterSteps} activeStep={initialState} />)
-    const step1 = wrapper.find('.StepCounterItem__bullet').at(0)  
+    const step1 = wrapper.find('.StepCounterItem__bullet').at(0)
+    const step2 = wrapper.find('.StepCounterItem__bullet').at(1)
+    const step3 = wrapper.find('.StepCounterItem__bullet').at(2)  
+    const step4 = wrapper.find('.StepCounterItem__bullet').at(3)
     const step5 = wrapper.find('.StepCounterItem__bullet').at(4)  
-    // step 1 and step 5 shouldnt affect the state if clicked, as only immedtate siblings can change state
+    // Only step 2 and step 4 should affect the state if clicked as only immedtate siblings can change state
+    // first test the 1st and 5th steps - they shouldnt change state 
     step1.simulate('click')
     expect(wrapper.state('activeStep')).toBe(initialState)
     step5.simulate('click')
     expect(wrapper.state('activeStep')).toBe(initialState)
+    // click the 4th - should change state
+    step4.simulate('click')
+    expect(wrapper.state('activeStep')).toBe(initialState+1)
+    // click the 3th - should change state back
+    step3.simulate('click')
+    expect(wrapper.state('activeStep')).toBe(initialState)
   })
+
  
 })
